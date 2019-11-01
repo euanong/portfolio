2fe4db2f351c00f8b2887068f484941649d1f74a
eohomegrownapps <euan.l.y.ong@gmail.com>
2019-11-01 22:59:18 +0000

    Update onload.js

--- onload.js
+++ onload.js
@@ -2,6 +2,6 @@ window.onload = function(){
 	init(); //waves.js
 	animate(); //waves.js
 	type(); //text.js
-	populateProjects(); //populateprojects.js
+	//populateProjects(); //populateprojects.js
 	doResize(); //resizer.js
 }
